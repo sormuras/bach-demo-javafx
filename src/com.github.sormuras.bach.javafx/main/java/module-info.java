eb5ef8a6f16467f0ab69d3c03e006897dc2a02e0
Christian Stein <sormuras@gmail.com>
2019-12-12 13:52:50 +0100

    Upgrade JavaFX to 14-ea+4

--- src/com.github.sormuras.bach.javafx/main/java/module-info.java
+++ src/com.github.sormuras.bach.javafx/main/java/module-info.java
@@ -1,8 +1,8 @@
 // --main-class com.github.sormuras.bach.javafx.App
 module com.github.sormuras.bach.javafx {
-  requires javafx.base;
-  requires javafx.controls;
-  requires javafx.graphics;
+  requires javafx.base /*14-ea+4*/;
+  requires javafx.controls /*14-ea+4*/;
+  requires javafx.graphics /*14-ea+4*/;
 
   exports com.github.sormuras.bach.javafx to javafx.graphics;
 }
