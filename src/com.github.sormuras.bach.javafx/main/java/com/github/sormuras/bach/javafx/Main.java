f0335c280b09d9a2defcca5af155400f2c19532b
Christian Stein <sormuras@gmail.com>
2020-03-14 16:53:27 +0100

    Rename main entry-point to match Bach's convention

--- src/com.github.sormuras.bach.javafx/main/java/com/github/sormuras/bach/javafx/Main.java
+++ src/com.github.sormuras.bach.javafx/main/java/com/github/sormuras/bach/javafx/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class Main extends Application {
 
   @Override
   public void start(Stage stage) {
