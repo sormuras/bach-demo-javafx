d8df93f3ed1895dccd9cdef53c7ebcc25eb005ff
Christian Stein <sormuras@gmail.com>
2020-12-05 21:39:56 +0100

    Upgrade to Bach 16-ea latest annotation API

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -1,31 +1,28 @@
-import com.github.sormuras.bach.ProjectInfo;
-import com.github.sormuras.bach.ProjectInfo.Library;
-import com.github.sormuras.bach.ProjectInfo.Library.Searcher;
-import com.github.sormuras.bach.ProjectInfo.Main;
-import com.github.sormuras.bach.ProjectInfo.Tweak;
-import com.github.sormuras.bach.module.ModuleSearcher.JavaFXSearcher;
+import build.JavaFX_16_ea_Modules;
+import com.github.sormuras.bach.project.Feature;
+import com.github.sormuras.bach.project.ProjectInfo;
+import com.github.sormuras.bach.project.ProjectInfo.ExternalModules;
+import com.github.sormuras.bach.project.ProjectInfo.Tweak;
 
 @ProjectInfo(
     name = "bach-javafx",
     version = "1",
-    library = @Library(searchers = {@Searcher(with = JavaFXSearcher.class, version = "16-ea+4")}),
-    main =
-        @Main(
-            generateCustomRuntimeImage = true,
-            tweaks = {
-              @Tweak(
-                  tool = "jar(com.github.sormuras.bach.javafx)",
-                  args = {"--main-class", "com.github.sormuras.bach.javafx.Main"}),
-              @Tweak(
-                  tool = "jlink",
-                  args = {
-                    "--launcher",
-                    "bach-javafx=com.github.sormuras.bach.javafx/com.github.sormuras.bach.javafx.Main",
-                    "--no-header-files",
-                    "--no-man-pages",
-                    "--strip-debug"
-                  }),
-            }))
-module build {
+    features = Feature.GENERATE_CUSTOM_RUNTIME_IMAGE,
+    externalModules = @ExternalModules( lookups = JavaFX_16_ea_Modules.class ),
+    tweaks = {
+        @Tweak(
+            tool = "jar(com.github.sormuras.bach.javafx)",
+            args = {"--main-class", "com.github.sormuras.bach.javafx.Main"}),
+        @Tweak(
+            tool = "jlink",
+            args = {
+                "--launcher",
+                "bach-javafx=com.github.sormuras.bach.javafx/com.github.sormuras.bach.javafx.Main",
+                "--no-header-files",
+                "--no-man-pages",
+                "--strip-debug"
+            }),
+    })
+open module build {
   requires com.github.sormuras.bach;
 }
