eeb716002aa64817e6797bfdc6439006afce262d
Christian Stein <sormuras@gmail.com>
2020-12-05 21:39:23 +0100

    Upgrade to JavaFX 16-ea+5
    Thanks @FDelporte (-:

---
+++ .bach/build/build/JavaFX_16_ea_Modules.java
@@ -0,0 +1,9 @@
+package build;
+
+import com.github.sormuras.bach.project.ModuleLookup;
+
+public class JavaFX_16_ea_Modules extends ModuleLookup.JavaFX {
+  public JavaFX_16_ea_Modules() {
+    super("16-ea+5");
+  }
+}
