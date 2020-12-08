594cdeb5adac2aea796570fc12593fba0aba57d7
Christian Stein <sormuras@gmail.com>
2020-12-08 13:02:54 +0100

    Use Bach's default project configuration

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -1,25 +1,10 @@
 import com.github.sormuras.bach.project.Feature;
 import com.github.sormuras.bach.project.ProjectInfo;
-import com.github.sormuras.bach.project.ProjectInfo.Tweak;
 
 @ProjectInfo(
-    name = "bach-javafx",
     version = "1",
-    features = Feature.GENERATE_CUSTOM_RUNTIME_IMAGE,
-    tweaks = {
-        @Tweak(
-            tool = "jar(com.github.sormuras.bach.javafx)",
-            args = {"--main-class", "com.github.sormuras.bach.javafx.Main"}),
-        @Tweak(
-            tool = "jlink",
-            args = {
-                "--launcher",
-                "bach-javafx=com.github.sormuras.bach.javafx/com.github.sormuras.bach.javafx.Main",
-                "--no-header-files",
-                "--no-man-pages",
-                "--strip-debug"
-            }),
-    })
+    features = { Feature.GENERATE_CUSTOM_RUNTIME_IMAGE }
+)
 module build {
   requires com.github.sormuras.bach;
   provides com.github.sormuras.bach.project.ModuleLookup with build.JavaFX_16_ea_Modules;
