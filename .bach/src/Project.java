ba8ac6626451c04b88367e183b5e966fb9934fbb
Christian Stein <sormuras@gmail.com>
2024-04-23 11:03:07 +0200

    Use built-in image compiler

--- .bach/src/Project.java
+++ .bach/src/Project.java
@@ -1,8 +1,6 @@
 import run.bach.Bach;
-import run.bach.ToolCall;
 import run.bach.ToolRunner;
 import run.bach.workflow.Builder;
-import run.bach.workflow.Cleaner;
 import run.bach.workflow.Structure;
 import run.bach.workflow.Workflow;
 
@@ -14,11 +12,12 @@ record Project(Workflow workflow) implements Builder {
     var folders = Bach.Folders.ofCurrentWorkingDirectory();
     var main =
         new Structure.Space(
-            "main",
-            0,
-            "demo/demo.Main",
-            new Structure.DeclaredModule(
-                folders.root("demo"), folders.root("demo", "module-info.java")));
+                "main",
+                0,
+                "demo=demo/demo.Main",
+                new Structure.DeclaredModule(
+                    folders.root("demo"), folders.root("demo", "module-info.java")))
+            .with(Structure.Space.Flag.IMAGE);
     var runner = ToolRunner.ofSystem();
     var spaces = new Structure.Spaces(main);
     var workflow = new Workflow(folders, new Structure(basics, spaces), runner);
@@ -28,24 +27,5 @@ record Project(Workflow workflow) implements Builder {
   @Override
   public boolean builderShouldInvokeCleanBeforeCompile() {
     return true;
-  }
-
-  @Override
-  public void build() {
-    Builder.super.build();
-    link();
-  }
-
-  void link() {
-    var folders = workflow.folders();
-    var main = workflow.structure().spaces().space("main");
-    var image = folders.out(main.name(), "image");
-    var link =
-        ToolCall.of("jlink")
-            .add("--output", image)
-            .add("--launcher", "demo=demo")
-            .add("--add-modules", main.modules().names(","))
-            .add("--module-path", main.toRuntimeSpace().toModulePath(folders).orElse("."));
-    run(link);
   }
 }
