cf3f783e4ce6cc9c9811aa2d85de0fd00b61e577
Christian Stein <sormuras@gmail.com>
2024-04-25 18:09:45 +0200

    Update to use latest `run.bach` submodule

--- .bach/src/Project.java
+++ .bach/src/Project.java
@@ -1,31 +1,21 @@
-import run.bach.Bach;
-import run.bach.ToolRunner;
 import run.bach.workflow.Builder;
-import run.bach.workflow.Structure;
 import run.bach.workflow.Workflow;
 
 record Project(Workflow workflow) implements Builder {
-  static final String VERSION = System.getProperty("--project-version", "0-ea");
-
   static Project ofCurrentWorkingDirectory() {
-    var basics = new Structure.Basics("bach-demo-javafx", VERSION);
-    var folders = Bach.Folders.ofCurrentWorkingDirectory();
-    var main =
-        new Structure.Space(
-                "main",
-                0,
-                "demo=demo/demo.Main",
-                new Structure.DeclaredModule(
-                    folders.root("demo"), folders.root("demo", "module-info.java")))
-            .with(Structure.Space.Flag.IMAGE);
-    var runner = ToolRunner.ofSystem();
-    var spaces = new Structure.Spaces(main);
-    var workflow = new Workflow(folders, new Structure(basics, spaces), runner);
-    return new Project(workflow);
+    return new Project(
+        Workflow.ofCurrentWorkingDirectory()
+            .withName("bach-demo-javafx")
+            .withVersion("99")
+            .withMainSpace(
+                main ->
+                    main.withModule("demo", "demo/module-info.java")
+                        .withLauncher("demo=demo/demo.Main")
+                        .withCompileRuntimeImage()));
   }
 
   @Override
-  public boolean builderShouldInvokeCleanBeforeCompile() {
+  public boolean builderDoesCleanAtTheBeginning() {
     return true;
   }
 }
